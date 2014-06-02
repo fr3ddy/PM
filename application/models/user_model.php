f0dadf3c530f0d53eec8b567b37808a31dc19aef
Patrick <patrick.voirol@arcor.de>
2014-06-02 15:19:33 +0200

    Mitarbeiter einer Abteilung werden ausgegeben

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -122,6 +122,7 @@ class User_Model extends CI_Model {
             $data[$i]["abteilungsleiter"] = $this -> gibBenutzerdatenID($row -> Abteilungsleiter);
             $i++;
         }
+        return $data;
     }
 
     function gibAbteilung($ID) {
@@ -130,6 +131,16 @@ class User_Model extends CI_Model {
         return array("AbteilungsID" => $row -> ID, "Abteilungsname" => $row -> Abteilungsname, "Abteilungsleiter" => $this -> gibBenutzerdatenID($row -> Abteilungsleiter), "Bereich" => $row -> Bereich);
     }
 
+    function gibAbteilungsmitarbeiter($ID) {
+        $query = $this -> db -> query("SELECT * FROM Benutzer WHERE Abteilung = " . $ID);
+        foreach ($query->result() as $row) {
+            $data[$i]["benutzername"] = $row -> Benutzername;
+            $data[$i]["id"] = $row -> ID;
+            $i++;
+        }
+
+        return $data;
+    }
 
     function aendereAbteilungsLeiter($AbteilungsID, $MitarbeiterID) {
         $query = $this -> db -> query("UPDATE Abteilungen SET Abteilungsleiter = " . $MitarbeiterID . " WHERE ID = " . $AbteilungsID);
