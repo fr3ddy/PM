694321a44a80fcfd2ea1c142c9a4787072e6abe8
Patrick <patrick.voirol@arcor.de>
2014-06-04 16:10:30 +0200

    bugfix

--- application/models/konfig_model.php
+++ application/models/konfig_model.php
@@ -19,7 +19,7 @@ class Konfig_Model extends CI_Model {
     function gibStrategien() {
         $query = $this -> db -> get('Strategien');
         $id = 0;
-		$data = array();
+        $data = array();
         foreach ($query->result() as $row) {
             $data[$i]["ID"] = $row -> ID;
             $data[$i]["Bezeichnung"] = $row -> Bezeichnung;
