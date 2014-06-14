a8125424d64c030659e37f2789acc28994266d72
Patrick <patrick.voirol@arcor.de>
2014-06-14 12:27:17 +0200

    strategieProjekte

--- application/models/projekte_model.php
+++ application/models/projekte_model.php
@@ -647,7 +647,7 @@ AND Plan.ProjektID = ProjektAmort.ID");
         return $data;
     }
 
-    function gibGesamtKosten($ProjektID) {        $this -> db -> where('ID', $ProjektID);
+    function gibGesamtKosten($ProjektID) {
         $this -> db -> where('ID', $ProjektID);
         $projektKostenQuery = $this -> db -> get('ProjektKosten');
         $projektKosten = $projektKostenQuery -> first_row();
@@ -659,6 +659,29 @@ AND Plan.ProjektID = ProjektAmort.ID");
     function loescheProjektAusPlan($ProjektID) {
         $this -> db -> where("ProjektID", $ProjektID);
         $this -> db -> delete("Plan");
+    }
+
+    function strategieProjekte() {
+        $data = array();
+        $query = $this -> db -> query("SELECT Plan.ProjektID, ProjektAllgemein.Titel, ProjektAmort.Gewinn, Strategien.ID as StrategieID, Strategien.Bezeichnung
+FROM Plan, ProjektAllgemein, ProjektAmort, Strategien
+WHERE Plan.ProjektID = ProjektAllgemein.ID 
+AND Plan.ProjektID = ProjektAmort.ID
+AND ProjektAllgemein.Strategie = Strategien.ID");
+
+        foreach ($query->result() as $row) {
+            if (isset($data[$row -> StrategieID])) {
+                $data[$row -> StrategieID]["Strategiesinformationen"]["Gewinn"] = $data[$row -> StrategieID]["Strategiesinformationen"]["Gewinn"] + $row -> Gewinn;
+                $data[$row -> StrategieID]["Strategiesinformationen"]["Kosten"] = $data[$row -> StrategieID]["Strategiesinformationen"]["Kosten"] + $this -> gibGesamtKosten($row -> ProjektID);
+            } else {
+                $data[$row -> StrategieID]["Strategiesinformationen"]["Gewinn"] = $row -> Gewinn;
+                $data[$row -> StrategieID]["Strategiesinformationen"]["Kosten"] = $this -> gibGesamtKosten($row -> ProjektID);
+            }
+            $data[$row -> StrategieID]["Strategiesinformationen"]["Strategiename"] = $row -> Bezeichnung;
+            $data[$row -> StrategieID]["Projekte"][$row -> ProjektID] = array("ID" => $row -> ProjektID, "Titel" => $row -> Titel);
+
+        }
+        return $data;
     }
 
 }
